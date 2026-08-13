e96f5f56c010cb258387aafd9d42c75c39c5a0be
Ryan Dalcin <dalcinryan0123@gmail.com>
2026-08-12 23:54:33 -0300

    Versao boa

--- js/supabase.js
+++ js/supabase.js
@@ -5,4 +5,10 @@ const SUPABASE_URL = "https://gucxwitkjmwngzorvchb.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1Y3h3aXRram13bmd6b3J2Y2hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1NTY0OTksImV4cCI6MjEwMjEzMjQ5OX0.J1vKsxqwsvlxZEyfgQdTz1DMYEfl-3CYMN18SE3TtPc";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
